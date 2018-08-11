2d15ea1c195340b81467e5399e504da5b3be8a69
Nicolas Even <n-e@users.noreply.github.com>
2018-08-11 18:09:35 +0200

    Update download.sh because of a change on the FFC website

--- download.sh
+++ download.sh
@@ -8,7 +8,7 @@ mkdir -p $FOLDER
 cd $FOLDER
 
 FFC='http://www.auvergnerhonealpescyclisme.com/route/calendrier'
-FFCLOISIR='http://auvergnerhonealpescyclisme.com/cyclisme-pour-tous/calendrier-resultats'
+FFCLOISIR='http://auvergnerhonealpescyclisme.com/cyclisme-pour-tous/calendrier'
 FSGT69='http://www.cyclismerhonefsgt.fr/calendrier/'
 FSGT42='http://www.fsgt42.com/cyclisme/index.php?page=listecourses'
 FSGT71='http://www.fsgt71velo.fr/2018/calendrier/calendrier.html'
